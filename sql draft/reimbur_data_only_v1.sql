9cede1fb3dc801f6d33f5c4647e326975f1e8345
versehe <verse_he@outlook.com>
2016-04-21 13:17:56 -0400

    add file

---
+++ sql draft/reimbur_data_only_v1.sql
@@ -0,0 +1,123 @@
+--------------------------------------------------------
+--  File created - Wednesday-November-11-2015   
+--------------------------------------------------------
+REM INSERTING into CLIENT
+SET DEFINE OFF;
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (1,'Ulrike Co,ltd','Güntzelstrasse 21
+54675 Nusbaum','Gärtner');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (2,'ConspiracyInsider.de','Brandenburgische Str 29
+55606 Otzweiler','Andreas Kastner');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (3,'Superior Interior Design','Motzstr. 41
+55413 Weiler bei Bingen','Doreen König');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (4,'SportsWorm.de','Karl-Liebknecht-Strasse 39
+24568 Kaltenkirchen','Mathias Wulf');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (5,'Marianne','Knesebeckstrasse 34 
+57641 Oberlahr','Knesebeckstrasse');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (6,'Handy Dan','Alt Reinickendorf 24
+87520 Sonthofen','Lea Schulze');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (7,'Munich Noodle Co,ltd','Rhinstrasse 4
+80752 München','Luetzowplatz 9
+57518 Alsdorf');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (8,'Munich Sausage','Rhinstrasse 45 
+80752 München','Claudia Winkel
+');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (9,'Wächtersbach Drinking Water','Konstanzer Strasse 43
+63607 Wächtersbach','Susanne Nussbaum');
+Insert into CLIENT (CLIENT_ID,CLIENT_NAME,LOCATION,CONTACT_NAME) values (10,'Jahnstrasse Beer','Jahnstrasse 66
+83090 Bad Endorf','Sabrina Zweig');
+REM INSERTING into EMPLOYEE
+SET DEFINE OFF;
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (1,5,'Llewelyn Alessio','847 8th Street 
+Bridgeport, CT 06606','+1-202-555-0186','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (2,9,'Phillipp Eberhart','Nuernbergerstrasse 64
+ 26676 Barßel','04409 40 65 43','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (3,17,'Tom Loewe','Karl-Liebknecht-Strasse 55
+ 27367 Sottrum','04205 44 78 38','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (4,14,'Klaus Schmitz','Ansbacher Strasse 77 
+54472 Burgen','06534 22 78 16','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (5,1,'Lisa Glockner','Nuernbergerstrasse 77 
+23834 Bad Oldesloe','04531 95 28 14','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (6,1,'Martina Schwartz','Brandenburgische Str. 87 
+75438 Knittlingen','07043 51 91 80','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (7,2,'Maria Cole','Rudolstaedter Strasse 93 
+63739 Aschaffenburg','06021 18 76 62','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (8,2,'Klaus Koertig','Hedemannstasse 52 
+79254 Oberried','07602 61 26 47','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (9,3,'Silke Faust','Fasanenstrasse 67 
+22549 Hamburg Osdorf','040 71 26 63','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (10,4,'Andreas Kuhn','Luckenwalder Strasse 85 
+31188 Holle  ','05063 71 63 94','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (11,6,'Ralf Gerste','Sömmeringstr. 54 
+76774 Leimersheim','07272 66 55 35','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (12,7,'Radzimierz Kalinowski','Alsterkrugchaussee 16 
+91184 Büchenbach','09122 58 54 65','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (13,8,'Li Hsu','Kantstrasse 12 
+95404 Bayreuth','0921 36 42 30','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (14,9,'Rhys Burton','Lützowplatz 60 
+54441 Taben-Rodt','06582 83 82 98','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (15,10,'Tilly Vadeboncoeur','Invalidenstrasse 4 
+76835 Hainfeld','06323 80 42 58','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (16,11,'Wictor Hermansson','Mühlenstrasse 96 
+97204 Höchberg','0931 11 96 07','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (17,12,'Menegilda Baggins','Paul-Nevermann-Platz 21 
+97795 Schondra','09744 96 23 64','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (18,13,'Kósa Piroska','Rankestraße 14 
+82335 Berg','08433 17 90 14','F',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (19,16,'Gabriele Unger','An der Alster 37 
+23968 Alt Jassewitz','038428 39 85','M',null);
+Insert into EMPLOYEE (EMPLOYEE_ID,TEAM_ID,NAME,ADDRESS,PHONE_NUMBER,GENDER,MANAGER_ID) values (20,15,'Sebastian Eberhardt','Oldesloer Strasse 95 
+07902 Schleiz','036647 73 51','F',null);
+REM INSERTING into TEAM
+SET DEFINE OFF;
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (1,'MARKETING_A');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (2,'MARKETING_B');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (3,'MARKETING_C');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (4,'MARKETING_OUTSOURCE');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (5,'SEALING_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (6,'I_AM_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (7,'TRAVEL_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (8,'USA_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (9,'INDIAN_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (10,'CHINESE_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (11,'MANAGEMENT_A');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (12,'MANAGEMENT_B');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (13,'ACCOUNTING_A');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (14,'ACCOUNTING_B');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (15,'HR');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (16,'GERMAN_ENGINEER');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (17,'FREELANCE');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (18,'COUNSULTANT_TEAM');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (19,'SUPPORT_TEAM_A');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (20,'SUPPORT_TEAM_B');
+Insert into TEAM (TEAM_ID,TEAM_NAME) values (21,'SUPPORT_TEAM_C');
+REM INSERTING into TRAVEL_AGENT
+SET DEFINE OFF;
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (1,'SOMEWHERE IN EUROPE','BOOKING','123-432-3231');
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (2,null,'NO_AGENT',null);
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (3,'SOMEWHERE IN ASIA','AGODA','323-932-3431');
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (4,'SOMEWHERE IN MIDDLEEAST','CRUEL_TRAVEL_COMPANY','999-432-2222');
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (5,'SOMEWHERE IN THAILAND','CURRUPTION','737-321-4444');
+Insert into TRAVEL_AGENT (AGENT_ID,ADDRESS,NAME,PHONE_NUMBER) values (6,'SOMEWHERE IN ANTARTICA','SICK','321-556-7777');
+REM INSERTING into TRAVEL_BUDGET
+SET DEFINE OFF;
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (1,'2015',1300000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (2,'2015',1500000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (3,'2015',1400000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (4,'2015',3000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (5,'2015',2000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (6,'2015',2900000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (7,'2015',1500000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (8,'2015',100000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (9,'2015',2000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (10,'2015',800000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (11,'2015',5000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (12,'2015',2000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (13,'2015',1000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (14,'2015',1000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (15,'2015',9000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (16,'2015',7000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (17,'2015',1000000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (18,'2015',2300000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (19,'2015',2300000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (20,'2015',2300000);
+Insert into TRAVEL_BUDGET (TEAMID,YEAR,AMOUNT) values (21,'2015',500000);
